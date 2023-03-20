86d6dfdfe8d002cfae535535b6c0aa57e20686ca
Evgeny Koshkin <wylde.kurt@gmail.com>
2023-03-20 15:14:46 +0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Evgeny Koshkin on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
